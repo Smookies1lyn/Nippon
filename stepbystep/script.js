4585d50d8f7ee0306d9d9cc9dc0de5024d707504
Emilie232000 <148345068+Emilie232000@users.noreply.github.com>
2024-02-28 11:22:42 +0100

    Add files via upload

---
+++ stepbystep/script.js
@@ -0,0 +1,21 @@
+const kontaktKnap = document.getElementById("kontaktKnap")
+
+kontaktKnap.addEventListener('mouseover', () => {
+  kontaktKnap.style.backgroundColor = '#98AF77';
+});
+
+kontaktKnap.addEventListener('mouseout', () => {
+  kontaktKnap.style.backgroundColor = '';
+});
+
+
+
+const stepKnap = document.getElementById("stepKnap")
+
+stepKnap.addEventListener('mouseover', () => {
+  stepKnap.style.backgroundColor = '#98AF77';
+});
+
+stepKnap.addEventListener('mouseout', () => {
+  stepKnap.style.backgroundColor = '';
+});
